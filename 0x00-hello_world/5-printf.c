e30d62c0bb3a1d53ffd08ffaab7485321508d117
badrxd <badrxdxd@gmail.com>
2023-03-16 22:34:57 +0100

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - function to print the text
+*
+* Return: return number 0
+*/
+int main (void)
+{
+	printf('%s\n',"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
