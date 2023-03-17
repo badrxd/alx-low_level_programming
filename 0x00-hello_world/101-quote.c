903cc4799754e2a4def921b4d0645dd7a6406540
badrxd <badrxdxd@gmail.com>
2023-03-17 01:51:53 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	fputs("nd that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	fputs("nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
