a32bcc626914eb09136e190214a7192af5e21412
badrxd <badrxdxd@gmail.com>
2023-03-17 18:49:30 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - fun 
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+        char a = 'B';
+        char b;
+        
+        b = tolower(a);
+        putchar(b);
+        return(0);
+}}
