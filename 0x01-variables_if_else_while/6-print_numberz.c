056f3eabcf951799276dd7e0e5f879d0ec00cd53
badrxd <badrxdxd@gmail.com>
2023-03-17 23:15:26 +0100

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * man - fun
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9 ; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
