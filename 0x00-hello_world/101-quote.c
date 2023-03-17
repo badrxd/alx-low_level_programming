734e8b92d213d271faf9f3be98159ca3b9048639
badrxd <badrxdxd@gmail.com>
2023-03-17 01:50:38 +0100

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - function for quote
+ *
+ * Return: return 1
+ */
+int main(void)
+{
+	fputs("nd that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	return (1);
+}
