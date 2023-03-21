34936433576f69a1506f7fc8f8005d5145887e8d
badrxd <badrxdxd@gmail.com>
2023-03-21 21:20:08 +0000

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+* print_sign - fun
+* @n: variable
+*
+* Return: 0 or 1
+*/
+int print_sign(int n)
+{
+	int a;
+
+	if (n > 0)
+	{
+		_putchat('+');
+		a = 1;
+	}
+	else if (n ==  0)
+	{
+		_putchar('0');
+		a = 0;
+	}
+	else
+	{
+		_putchar('-');
+		a = -1;
+	}
+	return (a);
+}
