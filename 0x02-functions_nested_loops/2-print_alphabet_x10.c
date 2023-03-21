c04a0ff4c7fd263383b08f91f51f3d6257eea581
badrxd <badrxdxd@gmail.com>
2023-03-21 17:37:30 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - fun
+*/
+void print_alphabet_x10(void)
+{
+	char lt;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (lt = 'a'; lt <= 'z'; lt++)
+		{
+			_putchar(lt);
+		}
+	}
+	putchar('\n');
+}
