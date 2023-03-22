a2a0501d791b273be261b4d0746d472001c857bb
badrxd <badrxdxd@gmail.com>
2023-03-22 01:51:57 +0000

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - fun
+ *
+ * @n: input .
+ *
+ * Return: no return.
+ */
+void print_to_98(int n)
+{
+	int c;
+
+	if (n > 98)
+		for (c = n; c > 98; c--)
+			printf("%d, ", c);
+	else
+		for (c = n ; c < 98; c++)
+			printf("%d, ", c);
+	printf("98\n");
+}
