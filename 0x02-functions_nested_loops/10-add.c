192511039f32f0295931356bbbaacce5d2b28683
badrxd <badrxdxd@gmail.com>
2023-03-22 01:38:50 +0000

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+include "main.h"
+/**
+ * add - fun
+ *
+ * @a: variable one.
+ * @b: variable two.
+ *
+ * Return: Add number one and number two.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
