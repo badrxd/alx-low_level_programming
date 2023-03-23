efd120373ccfbe143141cad35892ecc51ad522b1
badrxd <badrxdxd@gmail.com>
2023-03-23 17:37:47 +0000

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_diagonal - fun
+* @n: variable
+* Retunr : no return
+*/
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	b = 0;
+
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b <= a; b++)
+		{
+			_putchar(' ')
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
