436bc90e4d61efa3e9819588c5a3e9b601f09f3f
badrxd <badrxdxd@gmail.com>
2023-03-23 21:16:48 +0000

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -17,10 +17,10 @@ void print_square(int size)
 	{
 		for (a = 0; a < size; a++)
 	{
-	for (b = 0; b < size; b++)
-	{
-		_putchar('#');
-	}
+			for (b = 0; b < size; b++)
+			{
+				_putchar('#');
+			}
 	_putchar('\n');
 	}
 	}
