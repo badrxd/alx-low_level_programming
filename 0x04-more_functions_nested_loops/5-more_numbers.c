9bbf65de5dea9db4c0df916b05b7be04ffba9691
badrxd <badrxdxd@gmail.com>
2023-03-23 16:25:17 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* more_numbers - fun
+*
+* Retunr : no return
+*/
+void more_numbers(void)
+{
+	int i;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (i = 0; i <= 13; i++)
+		{
+			_putchar('0' + i);
+		}
+		_putchar('\n');
+	}
+}
