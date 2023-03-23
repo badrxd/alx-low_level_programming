92f6bab91051d3bff46ca1c7e426527e2a3408f0
badrxd <badrxdxd@gmail.com>
2023-03-23 18:10:50 +0000

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,5 +10,14 @@ void print_diagonal(int n)
 	int b;
 
 	b = 0;
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b <= a; b++)
+		{
+			putchar(' ');
+		}
+		putchar(92);
+		putchar('\n');
+	}
 	_putchar('\n');
 }
