44eb32492cb68e87e05bd440aa3203f32d6121be
badrxd <badrxdxd@gmail.com>
2023-03-23 15:57:51 +0000

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_numbers - fun
+*
+* Retunr : no return
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
