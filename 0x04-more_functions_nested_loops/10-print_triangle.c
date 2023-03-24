3e5e2abe9cc2a4a685938519bb76ce19e85b4ed2
badrxd <badrxdxd@gmail.com>
2023-03-24 01:27:36 +0000

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
 */
 void print_triangle(int size)
 {
-	int a, b, c;
+	int a, b;
 
 	if (size <= 0)
 	{
@@ -15,17 +15,16 @@ void print_triangle(int size)
 	}
 	else
 	{
-		for (a = 0; a < size; a++)
+		for (a = 1; a <= size; a++)
 		{
-		for (b = 1; b < (size - a); b++)
-		{
-			_putchar(' ');
+			for (b = 1; b <= size; b++)
+			{
+				if ((a + b) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			 _putchar('\n');
 		}
-		for (c = 0; c <= a; c++)
-		{
-			_putchar('#');
-		}
-		}
-		_putchar('\n');
 	}
 }
