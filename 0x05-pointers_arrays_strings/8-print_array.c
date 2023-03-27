12f2313d69f2deecb1b50d812c3f3bd41d9a56ff
badrxd <badrxdxd@gmail.com>
2023-03-27 22:59:06 +0000

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,8 +16,8 @@ void print_array(int *a, int n)
 		printf("%d", a[i]);
 		if (i != (n - 1))
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar(44);
+			_putchar(32);
 		}
 	}
 	_putchar('\n');
