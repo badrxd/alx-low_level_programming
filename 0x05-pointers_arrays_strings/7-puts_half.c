b42d5dea9bdbc8dade726187b0b9c62387f46358
badrxd <badrxdxd@gmail.com>
2023-03-27 21:42:19 +0000

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
 * puts_half - function
 *@str: a pointer
@@ -8,13 +7,15 @@
 */
 void puts_half(char *str)
 {
-	int i, l, n = 0;
+	int i, l, n;
 
-	l = strlen(str);
+	l = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		l++;
 	if (l % 2 == 1)
-		n = ((l - 1) / 2) + 1;
+		n = ((l + 1) / 2);
 	else
-		n = l / 2;
+		n = (l / 2);
 	for (i = n; i < l; i++)
 	{
 		_putchar(str[i]);
