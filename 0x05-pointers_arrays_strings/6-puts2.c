2275a9d3d23f701bf7231c8e8b37cc101717e484
badrxd <badrxdxd@gmail.com>
2023-03-27 17:42:47 +0000

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* puts2 - function
+*@str: a pointer
+*
+*Return: no return
+*/
+void puts2(char *str)
+{
+	int i, l;
+
+	l = strlen(str);
+	for (i = 0; i < l; i = i + 2)
+	{
+		_putchar(str[i]);
+	}
+}
