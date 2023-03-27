7d438b5234515f36f05aa91f39a01216c02a9d4f
badrxd <badrxdxd@gmail.com>
2023-03-27 22:01:30 +0000

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_array - function
+* @a: a pointer
+* @n: a pointer
+*
+* Return: no return
+*/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+			printf(", ");
+	}
+	_putchar('\n');
+}
