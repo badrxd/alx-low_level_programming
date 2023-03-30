5c914efc757f2e8046d02baf129814c15420318a
badrxd <badrxdxd@gmail.com>
2023-03-30 06:26:06 +0000

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,37 @@
 #include "main.h"
+
+/**
+* cheak_Separators - fun
+* @c: pointer
+*
+* Return: value
+*/
+int cheak_Separators(char c)
+{
+	int i;
+	char b[] = " \n\t,;.!?\"(){}";
+
+	for (i = 0; i < 13; i++)
+	{
+		if (c == b[i])
+			return (1);
+	}
+	return (0);
+}
+
+/**
+* cheak_capitalizes - fun
+* @c: pointer
+*
+* Return: c
+*/
+int cheak_capitalizes(char c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	return (0);
+}
+
 /**
 * *cap_string - fun
 * @a: pointer
@@ -10,13 +43,11 @@ char *cap_string(char *a)
 	int i;
 
 	i = 0;
-	while (a[i] != '\0')
+	while (a[i])
 	{
-		if ((a[i] == 32 || a[i] == '\n' || a[i] == '\t' ||
-		 a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i]
-		 == '!' || a[i] == '?' || a[i] == '\"' || a[i] == '('
-		 || a[i] == ')' || a[i] == '{' || a[i] == '}')  &&
-		 (a[i + 1] >= 'a' && a[i + 1] <= 'z'))
+		if (i == 0 && cheak_capitalizes(a[i]))
+			a[i] = a[i] - 32;
+		if (cheak_Separators(a[i]) && cheak_capitalizes(a[i + 1]))
 			a[i + 1] = a[i + 1] - 32;
 		i++;
 	}
