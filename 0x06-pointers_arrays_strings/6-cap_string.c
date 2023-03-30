6f3991fc4bf5f21f67f7b8abed5f4afdecb1ef9a
badrxd <badrxdxd@gmail.com>
2023-03-30 04:23:55 +0000

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* *cap_string(char *) - fun
+* @a: pointer
+*
+* Return: a
+*/
+char *cap_string(char *a)
+{
+	int i;
+
+	i = 0;
+	while (a[i] != '\0')
+	{
+		if ((a[i] == 32 || a[i] == '\n' || a[i] == '\t' ||
+		 a[i] == ',' || a[i] == ';' || a[i] == '.' || a[i]
+		 == '!' || a[i] == '?' || a[i] == '"' || a[i] == '('
+		 || a[i] == ')' || a[i] == '{' || a[i] == '}')  &&
+		 (a[i + 1] >= 'a' && a[i + 1] <= 'z'))
+			a[i + 1] = *(a + i + 1) - 32;
+		i++;
+	}
+	return (a);
+}
