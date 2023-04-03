40004822c43d0779222447025f8e4936a302ecd5
badrxd <badrxdxd@gmail.com>
2023-04-03 17:31:23 +0000

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,12 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n; i++)
+	for (; n > 0; i++)
+	{
 		*(dest + i) = src[i];
+		n--;
+	}
 	return (dest);
 }
