211db932d03c58f865904a6f5a0ecedebcfa17f7
badrxd <badrxdxd@gmail.com>
2023-04-03 04:54:48 +0000

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* *_memset - fun
+* @s: array
+* @b: value
+* @n: value
+* Return: s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i < 0; i < n; i++)
+		*(s + i) = b;
+	return (s);
+}
