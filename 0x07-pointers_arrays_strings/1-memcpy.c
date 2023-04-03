5491da4132dda7f1fa0392c7b4aa4610887bb21b
badrxd <badrxdxd@gmail.com>
2023-04-03 05:15:09 +0000

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* *_memcpy - fun
+* @dest: array
+* @src: value
+* @n: value
+* Return: s
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = src[i];
+	return (dest);
+}
