24bd08d8c4edcd520531692353c05b8407550355
badrxd <badrxdxd@gmail.com>
2023-04-04 02:23:25 +0000

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,9 +13,12 @@ void print_diagsums(int *a, int size)
 	s1 = 0;
 	s2 = 0;
 	for (i = 0; i < size; i++)
+	{
 		s1 = s1 + a[i * size + i];
 	}
 	for (i = size - 1; i >= 0; i--)
+	{
 		s2 += a[i * size + (size - i - 1)];
+	}
 	printf("%d, %d\n", s1 ,s2);
 }
