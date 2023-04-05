b993df813969f7a37f6458e6c087224c5a8bf3d9
badrxd <badrxdxd@gmail.com>
2023-04-05 22:44:15 +0000

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* _sqrt - fun
+* @a: value
+* @b: value
+* Return: n
+*/
+int _sqrt(int a, int b)
+{
+	if ((b * b) == a)
+	{
+		return (b);
+	}
+	else
+	{
+		if ((b * b) > num)
+			return (-1);
+		else
+			return (_sqrt(a, b + 1));
+	}
+}
+
+/**
+* _sqrt_recursion - fun
+* @n: value
+* Return: n
+*/
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (_sqrt(n, 0));
+}
