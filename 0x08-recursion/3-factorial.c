9eecfd522144a57231bdd5425ca1cb02c727eb37
badrxd <badrxdxd@gmail.com>
2023-04-05 06:31:58 +0000

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* factorial - fun
+* @n: pointer
+* Return: i
+*/
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n <= 1)
+		return (1);
+	n = n + factorial(n - 1);
+	return (n);
+}
