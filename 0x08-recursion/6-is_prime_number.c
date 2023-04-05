178924bacc0367fe4727c3e957bd275b160f767c
badrxd <badrxdxd@gmail.com>
2023-04-05 22:56:04 +0000

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -10,9 +10,13 @@ int prime_number(int a, int b)
 	if (b < a)
 	{
 		if (a % b == 0)
+		{
 			return (0);
+		}
 		else
+		{
 			return (prime_number(a, b + 1));
+		}
 	}
 	else
 	{
@@ -29,7 +33,12 @@ int prime_number(int a, int b)
 int is_prime_number(int n)
 {
 	if (n <= 1)
+	{
 		return (0);
+	}
 	else
+	{
 		return (prime_number(n, 2));
+	}
 }
+
