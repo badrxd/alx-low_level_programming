0f31ab9ef6eed58b2399197fdb7879cc636987e4
badrxd <badrxdxd@gmail.com>
2023-04-05 05:53:07 +0000

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _strlen_recursion - fun
+* @s: pointer
+* Return: i
+*/
+int _strlen_recursion(char *s)
+{
+	int i;
+
+	i = 1;
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	i = i + _strlen_recursion(s);
+	return (i);
+}
