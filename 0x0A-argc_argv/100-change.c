c8e06a79a9be4cc7e2471b1de0c02420c0b26a3a
badrxd <badrxdxd@gmail.com>
2023-04-09 11:10:30 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,25 +13,22 @@ int main(int argc, char **argv)
 	int i, coins, cents, c_array[5] = {25, 10, 5, 2, 1};
 
 	coins = 0;
-	cents = atoi(argv[1]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	cents = atoi(argv[1]);
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
 
-	for (i = 0; i < 5 && cents >= 0; i++)
+	for (i = 0; i < 5; i++)
 	{
-	while (cents >= c_array[i])
-	{
-		coins++;
-		cents = cents - c_array[i];
-	}
+		coins = coins + (cents / c_array[i]);
+		cents = cents % c_array[i];
 	}
 	printf("%d\n", coins);
 	return (0);
