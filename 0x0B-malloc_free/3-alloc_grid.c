4b823abc0099f1c48ce043f1d481dd01fa756376
badrxd <badrxdxd@gmail.com>
2023-04-12 00:16:05 +0000

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,10 @@ int **alloc_grid(int width, int height)
 		arr[i] = (int *)malloc(height * sizeof(int));
 	}
 	if (arr == NULL)
+	{
+		free(arr);
 		return ('\0');
+	}
 	for (i = 0; i < width; i++)
 	{
 		for (j = 0; j < height; j++)
