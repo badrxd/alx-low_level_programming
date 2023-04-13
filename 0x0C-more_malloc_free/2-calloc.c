7955a464ac307d5ced381021635e68d6421cfbf4
badrxd <badrxdxd@gmail.com>
2023-04-13 22:19:31 +0000

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* _calloc - fun
+* @nmemb: value
+* @size: value
+*
+*Return: ptr
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i, sum;
+	char *ptr;
+
+	sum = nmemb * size;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(sum);
+	if (!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < sum ; i++)
+		ptr[i] = 0;
+	return (ptr);
+}
