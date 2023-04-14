17b8997071ecbf7e30f08dff9561a9398ebfc688
badrxd <badrxdxd@gmail.com>
2023-04-14 01:30:32 +0000

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,27 +1,61 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* _calloc - fun
-* @nmemb: value
-* @size: value
+* calc_len - fun
+* @st: value
+* Return: i
+*/
+int calc_len(char *st)
+{
+	int i;
+
+	i = 0;
+	while (st[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+* string_nconcat - fun
+* @s1: value
+* @s2: value
+* @n: value
 *
 *Return: ptr
 */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i, sum;
+	int i, j, len1;
 	char *ptr;
 
-	sum = nmemb * size;
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	ptr = malloc(sum);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n >= calc_len(s2))
+		len1 = calc_len(s1) + calc_len(s2);
+	else
+		len1 = calc_len(s1) + n;
+	ptr = malloc(len1 + 1);
 	if (!ptr)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	for (i = 0; i < sum ; i++)
-		ptr[i] = 0;
+	j = 0;
+	for (i = 0 ; i < len1 ; i++)
+	{
+		while (s1[j] != '\0')
+		{
+			ptr[i] = s1[j];
+			j++;
+			i++;
+			continue;
+		}
+	ptr[i] = s2[i - j];
+	}
+	ptr[i] = '\0';
 	return (ptr);
 }
