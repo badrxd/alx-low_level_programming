8752423ce882d089f06b28f11b0c046290a95b37
badrxd <badrxdxd@gmail.com>
2023-04-14 22:50:43 +0000

    100-realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,22 +12,25 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *ptr2;
-	int i;
+	unsigned int i;
 
+	if (new_size == 0 && ptr != NULL)
+		return (NULL);
+	if (ptr == NULL)
+		return (malloc(new_size));
 	if (new_size == old_size)
 		return (ptr);
-
-	if (new_size == 0)
-		return (NULL);
+	ptr2 =(malloc(new_size * sizeof(ptr)));
 
 	if (new_size > old_size || ptr == NULL)
-		ptr2 = malloc(new_size);
-
-	for (i = 0 ; i < old_size ; i++)
 	{
-		if (ptr == NULL)
-			return (ptr2);
-		*((char *)ptr2 + i) = *((char *)ptr + i);
+		for (i = 0 ; i < old_size ; i++)
+		{
+			if (ptr == NULL)
+				return (ptr2);
+			((char *)ptr2)[i] = ((char *)ptr)[i];
+		}
 	}
+	free(ptr);
 	return (ptr2);
 }
