63e4a1adb15692f155d4d0c5a8fd996baf09ee36
badrxd <badrxdxd@gmail.com>
2023-04-14 01:34:24 +0000

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,15 +1,61 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* string_nconcat - fun
-* @b: value
-*
-* Return: ptr
+* calc_len - fun
+* @st: value
+* Return: i
 */
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+int calc_len(char *st)
 {
-	void *ptr = malloc(b);
+	int i;
+
+	i = 0;
+	while (st[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+* string_nconcat - fun
+* @s1: value
+* @s2: value
+* @n: value
+*
+*Return: ptr
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i, j, len1;
+	char *ptr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n >= calc_len(s2))
+		len1 = calc_len(s1) + calc_len(s2);
+	else
+		len1 = calc_len(s1) + n;
+	ptr = malloc(len1 + 1);
 	if (!ptr)
-		exit(98);
+	{
+		free(ptr);
+		return (NULL);
+	}
+	j = 0;
+	for (i = 0 ; i < len1 ; i++)
+	{
+		while (s1[j] != '\0')
+		{
+			ptr[i] = s1[j];
+			j++;
+			i++;
+			continue;
+		}
+	ptr[i] = s2[i - j];
+	}
+	ptr[i] = '\0';
 	return (ptr);
 }
