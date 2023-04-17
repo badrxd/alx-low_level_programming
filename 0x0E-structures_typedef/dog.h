b85c58f88193976e67fb4a7c1051d747ceb1e63f
badrxd <badrxdxd@gmail.com>
2023-04-17 04:36:17 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define a new type struct dog with the following elements:
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner
+};
