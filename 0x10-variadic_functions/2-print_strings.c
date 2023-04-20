2d9626800321f08ac38e44e8b7ef677746c6d0c1
badrxd <badrxdxd@gmail.com>
2023-04-20 23:49:31 +0000

    2-print_strings.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* print_numbers - fun
+* @separator: val
+* @n: val
+*
+*Return: sum
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list lis;
+	char *str;
+	unsigned int i;
+
+
+	va_start(lis, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(lis, char*);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+		if (i < (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	va_end(lis);
+	printf("\n");
+}
--- 0x10-variadic_functions/main.c
+++ 0x10-variadic_functions/main.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 int main(void)
 {
-    print_numbers(", ", 4, 0, 98, -1024, 402);
+    print_strings(", ", 2, "Jay", "Django");
     return (0);
 }
