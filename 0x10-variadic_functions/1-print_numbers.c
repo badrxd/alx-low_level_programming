46933580e3e89564d3219e8ed3c0e1fcdad3af12
badrxd <badrxdxd@gmail.com>
2023-04-20 23:07:21 +0000

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+* print_numbers - fun
+* @separator: val
+* @n: val
+*
+*Return: sum
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list lis;
+	int num1 = 0;
+	unsigned int i;
+
+
+	va_start(lis, n);
+
+	for (i = 0; i < n; i++)
+	{
+		num1 = va_arg(lis, int);
+		printf("%d", num1);
+		if (i < n - 1 && eparator != NULL)
+			printf("%s", separator);
+	}
+	va_end(lis);
+	return (sum);
+}
