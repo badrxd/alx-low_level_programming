940e41b6f2ad301d32bdc135f3dd38c0f8dcec11
badrxd <badrxdxd@gmail.com>
2023-04-28 19:47:24 +0000

    ok

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,7 +13,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
+ *
  */
 typedef struct list_s
 {
@@ -21,6 +22,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
