58ec1fff5b51208bb2105600c72527a9eb40c8ab
badrxd <badrxdxd@gmail.com>
2023-05-08 21:13:34 +0100

    ok

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - fun
+ * @filename: A pointer
+ * @letters: The maximum number of bytes to be read from the file.
+ *
+ * Return: if the function succeeds, it returns
+ * the number of bytes read from the file and printed to
+ * the standard output. If an error occurs, it returns 0.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t re, wr, op;
+	char *buffer;
+
+	if (!filename)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (0);
+
+	op = open(filename, O_RDONLY);
+	re = read(op, buffer, letters);
+	wr = write(STDOUT_FILENO, buffer, re);
+
+	if (op == -1 || re == -1 || wr == -1 || re != wr)
+	{
+		free(buffer);
+		return (0);
+	}
+	free(buffer);
+	close(op);
+	return (wr);
+}
