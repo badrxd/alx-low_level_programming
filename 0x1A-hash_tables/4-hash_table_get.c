86ef8655a02a3c305b2490084ed8224f22b8f42f
badrxd <badrxdxd@gmail.com>
2023-07-07 18:37:23 +0100

    ok

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,9 +13,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
 	index = key_index((unsigned char *)key, ht->size);
-	if (!ht->array[index])
-		return(NULL);
 	node = ht->array[index];
 	while (node != NULL)
 	{
