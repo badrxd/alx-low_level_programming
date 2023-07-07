97cb7ae570e8138de704e85d4d7903eee9e91b61
badrxd <badrxdxd@gmail.com>
2023-07-07 19:14:15 +0100

    ok

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+/**
+* hash_table_print - f.
+* @ht: the hash table.
+*
+* Return: no return.
+*/
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i , j;
+	hash_node_t *node;
+
+	j = 0;
+	if (ht != NULL)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				if (j == 1)
+					printf(", ");
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				j = 1;
+			}
+		}
+		printf("}\n");
+	}
+}
