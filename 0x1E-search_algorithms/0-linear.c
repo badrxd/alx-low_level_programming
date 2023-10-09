c151897afb59ddb63b494f39f4e12a2e90d9d816
badrxd <badrxdxd@gmail.com>
2023-10-10 00:28:54 +0100

    task 0 and 1

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - finction for Linear search algorithm
+ * @array: pointer to array
+ * @size: size of array
+ * @value: value to search
+ *
+ * Return: index of value or -1 if value is not found
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+	printf("Value checked array[0] = [%d]\n", array[i]);
+	if (array[i] == value)
+		return (i);
+	}
+	return (-1);
+}
