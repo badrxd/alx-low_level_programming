43d66da08d8f2b13c2be2f8816522ecf7ded650d
badrxd <badrxdxd@gmail.com>
2023-10-10 20:08:41 +0100

    task 7. Jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+
+/**
+ * _print - function to print
+ * @array: pointer of the array
+ * @start: index of the first element
+ *
+ * Return: no return value
+ */
+void _print(int *array, int start)
+{
+	printf("Value checked array[%d] = [%d]\n", start, array[start]);
+}
+
+/**
+ * jump_search - search algorithm type jump search
+ * @array: pointer of the array
+ * @size: array size
+ * @value: value to search
+ *
+ * Return: index of value or -1 if not found
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	int start, end, cast_sz, i;
+
+	cast_sz = (int)size;
+	start = 0;
+	end = sqrt(size);
+
+	_print(array, start);
+	while (value > array[end] && start < cast_sz - 1)
+	{
+		start = end;
+		if (end > cast_sz - 1)
+			end = cast_sz;
+		else
+			end = end + sqrt(size);
+		_print(array, start);
+	}
+	printf("Value found between indexes [%d] and [%d]\n", start, end);
+
+	for (i = start; i <= end && i < cast_sz; i++)
+	{
+		_print(array, start);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,11 +1,14 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
 
 #endif
